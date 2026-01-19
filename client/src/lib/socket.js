7b909f7cb2a09f3159c23d427968dc04de39f39e
Reagan Vose v2 <reaganvosev2@RV-NTEGRA-MBP.local>
2026-01-19 15:29:17 +0000

    Update frontend to use production backend URL

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000", {
+export const socket = io("https://planning-poker-production-31f9.up.railway.app", {
   autoConnect: true
 });
