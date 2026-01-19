8a7a2d53882841880a9142156f6f8b4422ad00d3
Reagan Vose v2 <reaganvosev2@RV-NTEGRA-MBP.local>
2026-01-19 16:14:00 +0000

    Add Railway domains to CORS allowed origins

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const server = http.createServer(app);
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
+  "http://localhost:8080",
+  /https:\/\/.*\.railway\.app/,
   /https:\/\/.*\.github\.io\/planning-poker/
 ];
 
