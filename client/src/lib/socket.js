69237832054361921011451f4d3ed807e37d3970
Reagan Vose v2 <reaganvosev2@RV-NTEGRA-MBP.local>
2026-01-21 13:44:05 +0000

    updated server

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SERVER_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:4000";
+const SERVER_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "https://planning-poker-production-31f9.up.railway.app";
 
 export const socket = io(SERVER_URL, {
   autoConnect: true
